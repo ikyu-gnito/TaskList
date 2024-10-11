8a3c1a4829201a8253d60376ff4d3ea75b94e72e
ArtuSants <arturborba02@gmail.com>
2024-10-11 17:45:03 -0300

    Edit Cards
    agora da pra editar os dados dos cards
    
    adicionei também uma biblioteca pra formatar a data

--- script.js
+++ script.js
@@ -3,29 +3,65 @@ const $priorityInput = document.getElementById('priority');
 const $deadlineInput = document.getElementById('deadline');
 const $descriptionInput = document.getElementById('description');
 const $todoColumn = document.querySelector('#todoColumn .body');
+const $idInput = document.getElementById('idInput');
+const $createTitle = document.getElementById('createTitle');
+const $editTitle = document.getElementById('editTitle');
+const $addTask = document.getElementById('addTask');
+const $editTask = document.getElementById('editTask');
 
 // lista que vai receber os valores das tasks
 var todoList = [];
 
-function openModal() {
+function openModal(id) {
     $modal.style.display = "flex";
+
+    if (id){
+        $createTitle.style.display = "none";
+        $editTitle.style.display = "block";
+
+        $addTask.style.display = "none";
+        $editTask.style.display = "block";
+
+        const index = todoList.findIndex(function(task) {
+            return task.id;
+        });
+
+        const task = todoList[index];
+
+        $idInput.value = task.id;
+        $descriptionInput.value = task.description;
+        $priorityInput.value = task.priority;
+        $deadlineInput.value = task.deadline;
+    }else{
+        $createTitle.style.display = "block";
+        $editTitle.style.display = "none";
+
+        $addTask.style.display = "block";
+        $editTask.style.display = "none";
+    }
 }
 
 function closeModal() {
     $modal.style.display = "none";
+
+    $idInput.value = "";
+    $descriptionInput.value = "";
+    $priorityInput.value = "";
+    $deadlineInput.value = "";
 }
 
 function generateCard(){
     const todoListhtml = todoList.map(function(task){
+        const formatedDate = moment(task.deadline).format('DD/MM/YYYY')
         return `
-            <div class = "card">
+            <div class = "card" ondblclick ="openModal(${task.id})">
                 <div class = "info">
                     <b>Prioridade:</b>
                     <span>${task.priority}</span>
                 </div>
                 <div class = "info">
                     <b>Prazo:</b>
-                    <span>${task.deadline}</span>
+                    <span>${formatedDate}</span>
                 </div>
                 <div class = "info">
                     <b>Descrição:</b>
@@ -41,11 +77,30 @@ function generateCard(){
 // cria task e adiciona na lista
 function createTask() {
     const newTask = {
+        id: Math.floor(Math.random()*9999999999),
         priority: $priorityInput.value,
         deadline: $deadlineInput.value,
         description: $descriptionInput.value
     }
     todoList.push(newTask);
+
+    closeModal();
+    generateCard();
+}
+
+function updateTask(){
+    const Task = {
+        id: $idInput.value,
+        priority: $priorityInput.value,
+        deadline: $deadlineInput.value,
+        description: $descriptionInput.value
+    }
+
+    const index = todoList.findIndex(function(task) {
+        return task.id = $idInput.value;
+    });
+
+    todoList[index] = Task;
 
     closeModal();
     generateCard();
