e6c77a207e6bfd702c0f4cbb4ea37e3803504139
ArtuSants <arturborba02@gmail.com>
2024-10-17 15:20:34 -0300

    Atualizações
    add: delete function

--- script.js
+++ script.js
@@ -13,38 +13,34 @@ const $editTask = document.getElementById("editTask");
 var todoList = [];
 
 function openModal(id) {
-  $modal.style.display = "flex";
-
-  if (id) {
-    $createTitle.style.display = "none";
-    $editTitle.style.display = "block";
-
-    $addTask.style.display = "none";
-    $editTask.style.display = "block";
-
-    const index = todoList.findIndex(function (task) {
-      return task.id == id;
-    });
-
-    const task = todoList[index];
-
-    $idInput.value = task.id;
-    $descriptionInput.value = task.description;
-    $priorityInput.value = task.priority;
-    $deadlineInput.value = task.deadline;
-  } else {
-    $createTitle.style.display = "block";
-    $editTitle.style.display = "none";
-
-    $addTask.style.display = "block";
-    $editTask.style.display = "none";
-
-    $idInput.value = "";
-    $descriptionInput.value = "";
-    $priorityInput.value = "";
-    $deadlineInput.value = "";
+    $modal.style.display = "flex";
+  
+    if (id) {
+      $createTitle.style.display = "none";
+      $editTitle.style.display = "block";
+      $addTask.style.display = "none";
+      $editTask.style.display = "block";
+  
+      const task = todoList.find(task => task.id === id);
+  
+      if (task) {
+        $idInput.value = task.id;
+        $descriptionInput.value = task.description;
+        $priorityInput.value = task.priority;
+        $deadlineInput.value = task.deadline;
+      }
+    } else {
+      $createTitle.style.display = "block";
+      $editTitle.style.display = "none";
+      $addTask.style.display = "block";
+      $editTask.style.display = "none";
+  
+      $idInput.value = "";
+      $descriptionInput.value = "";
+      $priorityInput.value = "";
+      $deadlineInput.value = "";
+    }
   }
-}
 
 function closeModal() {
   $modal.style.display = "none";
@@ -56,61 +52,60 @@ function closeModal() {
 }
 
 function generateCard(task) {
-  const todoListhtml = todoList.map(function (task) {
-    const formatedDate = moment(task.deadline).format("DD/MM/YYYY");
+    const formatedDate = moment(task.deadline).format('DD/MM/YYYY');
     return `
-            <div class = "card" id="task-${task.id}" data-id="${task.id}" ondblclick ="openModal(${task.id})" draggable = "true" ondragstart = "drag(event)">
-                <div class = "info">
-                    <b>Prioridade:</b>
-                    <span>${task.priority}</span>
-                </div>
-                <div class = "info">
-                    <b>Prazo:</b>
-                    <span>${formatedDate}</span>
-                </div>
-                <div class = "info">
-                    <b>Descrição:</b>
-                    <span>${task.description}</span>
-                </div>
-            </div>
-        `;
-  });
-
-  $todoColumn.innerHTML = todoListhtml.join("");
+      <div class="card" id="task-${task.id}" data-id="${task.id}" draggable="true" ondragstart="drag(event)">
+        <div class="card-header">
+          <span class="edit-btn" onclick="openModal(${task.id})">Editar</span>
+          <span class="delete-btn" onclick="deleteTask(${task.id})"><i class="fa-solid fa-rectangle-xmark xmark"></i></span>
+        </div>
+        <div class="info">
+          <b>Prioridade:</b>
+          <span>${task.priority}</span>
+        </div>
+        <div class="info">
+          <b>Prazo:</b>
+          <span>${formatedDate}</span>
+        </div>
+        <div class="info">
+          <b>Descrição:</b>
+          <span>${task.description}</span>
+        </div>
+      </div>
+    `;
 }
 
 // cria task e adiciona na lista
 function createTask() {
-  const newTask = {
-    id: Math.floor(Math.random() * 9999999999),
-    priority: $priorityInput.value,
-    deadline: $deadlineInput.value,
-    description: $descriptionInput.value,
-    status: "todo",
-  };
-  todoList.push(newTask);
-
-  closeModal();
-  renderAllTasks();
-}
+    const newTask = {
+      id: Date.now(), // Usa timestamp como ID para garantir unicidade
+      priority: $priorityInput.value,
+      deadline: $deadlineInput.value,
+      description: $descriptionInput.value,
+      status: 'todo'
+    };
+    todoList.push(newTask);
+  
+    closeModal();
+    renderAllTasks();
+  }
 
 function updateTask() {
-  const Task = {
-    id: $idInput.value,
-    priority: $priorityInput.value,
-    deadline: $deadlineInput.value,
-    description: $descriptionInput.value,
-  };
-
-  const index = todoList.findIndex(function (task) {
-    return task.id == Number($idInput.value);
-  });
-
-  todoList[index] = Task;
-
-  closeModal();
-  renderAllTasks();
-}
+    const taskId = Number($idInput.value);
+    const index = todoList.findIndex(task => task.id === taskId);
+    
+    if (index !== -1) {
+      todoList[index] = {
+        ...todoList[index],
+        priority: $priorityInput.value,
+        deadline: $deadlineInput.value,
+        description: $descriptionInput.value
+      };
+    }
+  
+    closeModal();
+    renderAllTasks();
+  }
 
 function allowDrop(ev) {
   ev.preventDefault();
@@ -144,19 +139,21 @@ function updateTaskStatus(taskId, newStatus) {
 }
 
 function renderAllTasks() {
-  const todoTasks = todoList.filter((task) => task.status === "todo");
-  const inProgressTasks = todoList.filter(
-    (task) => task.status === "inProgress"
-  );
-  const doneTasks = todoList.filter((task) => task.status === "done");
+    const todoTasks = todoList.filter(task => task.status === 'todo');
+    const inProgressTasks = todoList.filter(task => task.status === 'inProgress');
+    const doneTasks = todoList.filter(task => task.status === 'done');
+  
+    document.querySelector('#todoColumn .body').innerHTML = todoTasks.map(generateCard).join('');
+    document.querySelector('#inProgressColumn .body').innerHTML = inProgressTasks.map(generateCard).join('');
+    document.querySelector('#doneColumn .body').innerHTML = doneTasks.map(generateCard).join('');
+  }
 
-  $todoColumn.querySelector("#todoColumn .body").innerHTML = todoTasks
-    .map(generateCard)
-    .join("");
-  $inProgressColumn.querySelector("#inProgressColumn .body").innerHTML = inProgressTasks
-    .map(generateCard)
-    .join("");
-  $doneColumn.querySelector("doneColumn .body").innerHTML = doneTasks
-    .map(generateCard)
-    .join("");
+  function deleteTask(id) {
+    if (confirm('Tem certeza que deseja excluir esta tarefa?')) {
+        const index = todoList.findIndex(task => task.id === id);
+        if (index !== -1) {
+            todoList.splice(index, 1);
+            renderAllTasks();
+        }
+    }
 }
